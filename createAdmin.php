c76e074dd443cd42e729d42f40a9383630b8ec66
Jack <jackmetcalf26@gmail.com>
2020-11-23 20:34:36 -0500

    modified:   createAdmin.php

--- createAdmin.php
+++ createAdmin.php
@@ -11,8 +11,21 @@
   <p>Create Admin</p>
 
   <?php
-  //need to verify that user is admin
 
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $position = $_POST['position'];
+
+  $message = "A new user is attempting to gain administrator privileges.\n\nTheir details are as follows\nName: $name\nEmail: $email\nPosition: $position\n\nThis is an automated message, please do not reply.";
+
+  mail("m230270@usna.edu", "Attempted Admin Access", $message);
+  //need to verify that user is admin
+  
+  //query for name, email, and position.
+  
+  //send email to courtney 
+
+  //courtney will redirect them to a new file to set up session variables if admitted.
 
 
 
