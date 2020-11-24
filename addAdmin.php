284bf87db4d0399f86308b498cfd5a176dc1f320
Jack <jackmetcalf26@gmail.com>
2020-11-23 20:35:53 -0500

    new file:   addAdmin.php 	new file:   adminSignup.html 	new file:   whitelist.txt

---
+++ addAdmin.php
@@ -0,0 +1,33 @@
+<?php
+  session_start();
+  if($_SESSION['user'] != '226618')
+    header("Location: welcome.html");
+?>
+<!DOCTYPE html>
+
+<html>
+<head lang="en"><meta charset = "utf-8">
+  <title>Add Admin</title>
+</head>
+<body>
+  <p>Add Administrator</p>
+  
+  <form method="POST" action="addAdmin.php">
+    <label for="email">Email: </label>
+    <input type="email" name="email" placeholder="Email Address" id="email">
+    <br>
+    <input type="submit" value="Submit">
+  </form>
+
+  <?php
+    $fp = fopen("whitelist.txt", 'a');
+    if(!$fp)
+        echo "<p>Failed to open whitelist txt file!</p>\n";
+    fwrite($fp, $_POST['email'] + "\n");
+    
+  ?>
+
+  <a href="welcome.html"> Home </a> <br>
+
+</body>
+</html>
